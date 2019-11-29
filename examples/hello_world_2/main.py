bb8cd15bf14ca7d5733e7d6c7a6711f21e9a5f66
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2019-11-29 04:12:01 +0100

    Added data transfer to the js->py->js roundtrip.

--- examples/hello_world_2/main.py
+++ examples/hello_world_2/main.py
@@ -15,9 +15,9 @@ class App(ShowBase):
         self.ui.set_js_function('call_py', self.handler_js_to_py)
         self.ui.load_file('ui/main.html')
 
-    def handler_js_to_py(self):
-        print('Python called')
-        self.ui.exec_js_func('color_text_red')    
+    def handler_js_to_py(self, color):
+        print("Python handler called with '{}'".format(color))
+        self.ui.exec_js_func('color_text_red', color)    
 
 
 if __name__ == '__main__':
